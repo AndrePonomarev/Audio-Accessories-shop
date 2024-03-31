d2653f13575f9fc397f7039747827b96c7da4876
Пономарев Андрей <cr_ovr_web3@efko.ru>
2024-03-31 12:43:18 +0300

    реализован react Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/items";
+import ShopCart from "./components/ShopCart";
 
 class App extends React.Component {
   constructor(props) {
@@ -74,24 +76,37 @@ class App extends React.Component {
           price: '7527 Р',
           mark: '4.7'
         },
-      ]
+      ],
+      cart: [] // Добавим пустой массив для корзины покупок
     }
   }
 
   render() {
     return (
-      <div className="wrapper">
-        
-        <Header />
-        <p class="headphones">Наушники</p>
-        <Items items={this.state.items} />
-        <p class="headphones">Беспроводные Наушники</p>
-        <Items items={this.state.wireless} />
-        <Footer />
-
-      </div>
+      <Router>
+        <div className="wrapper">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home items={this.state.items} wireless={this.state.wireless} />} />
+            <Route path="/cart" element={<ShopCart cart={this.state.cart} />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     );
   }
+}
+
+// Компонент Home для отображения списка товаров
+function Home({ items, wireless }) {
+  return (
+    <div>
+      <p className="headphones">Наушники</p>
+      <Items items={items} />
+      <p className="headphones">Беспроводные Наушники</p>
+      <Items items={wireless} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <header>
             <div className="header">
-                <span className="logo">QPICK</span>
+                <span className="logo"> <Link to="/">QPICK</Link></span>
                 <ul className="nav">
                     <li><img src="./img/izbrannoe.png" alt="Избранное" /></li>
-                    <li><img src="./img/korzina.png" alt="Корзина" /></li>
+                    <Link to="/cart"><li><img src="./img/korzina.png" alt="Корзина" /></li></Link>
                 </ul>
             </div>
         </header>
